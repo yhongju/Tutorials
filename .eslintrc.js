274c8875aa4d8376c1badc224153eed88ec7a15a
Yi Hong-ju <to_hongju@pm.me>
2022-11-12 20:00:59 +0900

    Add version infos
    Ecma version to 2020 for optional chaining at eslintrc
    Node version for feature(maybe spread)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,6 @@
 module.exports = {
+  parserOptions: {
+    ecmaVersion: 2020, // For optional chaining
+  },
   extends: ["airbnb-base", "plugin:node/recommended", "prettier"], // "prettier" should be located at last
 }
