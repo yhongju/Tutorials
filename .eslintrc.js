aaa62640c27045f2e9b2a7316d0235a38126b96b
Yi Hong-ju <to_hongju@pm.me>
2022-11-19 15:04:32 +0900

    Change eslintrc's file format to json
    From .js

--- .eslintrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  parserOptions: {
-    ecmaVersion: 2020, // For optional chaining
-  },
-  extends: ["airbnb-base", "plugin:node/recommended", "prettier"], // "prettier" should be located at last
-}
