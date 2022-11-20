5f06409526f71a32d3b8b7cc469350c88977517c
Yi Hong-ju <to_hongju@pm.me>
2022-11-21 08:03:41 +0900

    Add explanation and Rename to camel case
    Rejected Promise object could be Promise.race's winner

--- Back-end_Part-01/Ch-04/Promise/YouTube-lecture/promiseRace.js
+++ Back-end_Part-01/Ch-04/Promise/YouTube-lecture/promiseRace.js
@@ -3,7 +3,7 @@
 
 /*
  * "Promise.race"
- * Winner takes it all
+ * Winner takes it all, even if "rejected"
  * But race WON'T BE FINISHED when all functions were returning
  */
 
@@ -14,46 +14,46 @@
 const fn1 = () =>
   new Promise((res, rej) => {
     setTimeout(() => {
-      res("Order 01 complete!")
-    }, 1000) // Maybe returned first order and only one
-  })
+      res("Order 01 complete!");
+    }, 1000); // Maybe returned first order and only one
+  });
 
 const fn2 = (valueMsg) => {
-  console.log(valueMsg)
+  console.log(valueMsg);
 
-  const flagForTest = false
+  const flagForTest = false;
 
   return new Promise((res, rej) => {
     setTimeout(() => {
       if (flagForTest) {
-        rej(new Error("!!! Order 02 REJECTED!!!"))
+        rej(new Error("!!! Order 02 REJECTED!!!"));
       } else {
-        res("Order 02 complete!")
+        res("Order 02 complete!");
       }
-    }, 3000)
-  })
-}
+    }, 3000);
+  });
+};
 
 const fn3 = (valueMsg) => {
-  console.log(valueMsg)
+  console.log(valueMsg);
 
   return new Promise((res, rej) => {
     setTimeout(() => {
-      res("Order 03 complete!")
-    }, 2000)
-  })
-}
+      res("Order 03 complete!");
+    }, 2000);
+  });
+};
 
 /*
  * Driving codes
  */
 
-console.log("Init...")
+console.log("Init...");
 
-console.time("Three functions")
+console.time("Three functions");
 // Functions as an array too
 // "ret" value will be first returned value
 Promise.race([fn1(), fn2(), fn3()]).then((ret) => {
-  console.log(ret)
-  console.timeEnd("Three functions")
-})
+  console.log(ret);
+  console.timeEnd("Three functions");
+});
