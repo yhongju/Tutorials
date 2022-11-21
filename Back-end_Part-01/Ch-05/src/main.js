dc79a991553ecca854710360a6abbd608ca31afe
Yi Hong-ju <to_hongju@pm.me>
2022-11-21 21:20:06 +0900

    Build basic frame of web blog posting server

--- Back-end_Part-01/Ch-05/src/main.js
+++ Back-end_Part-01/Ch-05/src/main.js
@@ -1,3 +1,34 @@
 // @ts-check
 
+/**
+ * Build very simple blog posting web service server
+ *
+ * Use RESTful api
+ */
+
 const http = require("http");
+const PORT = 4000;
+
+/**
+ * Post feature
+ *
+ * GET /posts
+ *
+ * [ID specified posts]
+ * GET /posts/:id
+ *
+ * POST /posts
+ */
+
+const server = http.createServer((req, res) => {
+  // Print user requested info
+  console.log("Requested URL:", req.url);
+
+  res.statusCode = 200;
+  res.end("Hello from SERVER!");
+});
+
+// Driver code
+server.listen(PORT, "localhost", () => {
+  console.log(`The server is listening at ${PORT} port...`);
+});
