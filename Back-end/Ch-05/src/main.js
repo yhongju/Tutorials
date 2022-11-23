c7f475ee55163d725b0ac5701d879c2dc780686f
Yi Hong-ju <to_hongju@pm.me>
2022-11-23 20:06:01 +0900

    Mod Ch05/main.js with URL parsing to RegExp

--- Back-end/Ch-05/src/main.js
+++ Back-end/Ch-05/src/main.js
@@ -1,31 +1,59 @@
 // @ts-check
+/* eslint-disable no-console */
 
 /**
  * Build very simple blog posting web service server
  *
- * Use RESTful api
+ * Use RESTful api with HTTP verb
  */
-
-const http = require("http");
-const PORT = 4000;
 
 /**
  * Post feature
  *
  * GET /posts
- *
- * [ID specified posts]
- * GET /posts/:id
- *
+ * GET /posts/:id -> ID specified posts
  * POST /posts
  */
 
-const server = http.createServer((req, res) => {
-  // Print user requested info
-  console.log("Requested URL:", req.url);
+const http = require("http");
 
-  res.statusCode = 200;
-  res.end("Hello from SERVER!");
+const PORT = 4000;
+
+const server = http.createServer((req, res) => {
+  // Process by HTTP verb and URL
+  // req.method, req.url
+
+  // Get id from capturing group
+  const POSTS_ID_REGEXP = /^\/posts\/([a-zA-Z0-9_]+)$/;
+
+  // [1] index is captured content
+  const capturedId = req.url && POSTS_ID_REGEXP.exec(req.url)?.[1];
+
+  if (req.method === "GET" && req.url === "/posts") {
+    // General get post request
+
+    // Send response for prevent "response hanging"
+    res.statusCode = 200; // [200 ~ 299] HTTP successful response status code
+    res.end("List of posts");
+  } else if (req.method === "GET" && capturedId) {
+    // ID specifying get post request
+
+    // Print captured id
+    console.log(`Requested ID: ${capturedId}`);
+
+    res.statusCode = 200;
+    res.end("List of id specified posts");
+  } else if (req.method === "POST" && req.url === "/posts") {
+    // General post the post request
+
+    res.statusCode = 200;
+    res.end("Create the posts");
+  } else {
+    // Otherwise
+
+    res.statusCode = 404; // [400 ~ 499] HTTP client error status code
+    res.end("404 Not found");
+  }
 });
 
 // Driver code
