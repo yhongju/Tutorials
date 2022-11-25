79c177f7ea369bd661b3bf73de08ae4d79782db7
Yi Hong-ju <to_hongju@pm.me>
2022-11-25 17:47:25 +0900

    Update Ch05 main code with posting feature

--- Back-end/Ch-05/src/main.js
+++ Back-end/Ch-05/src/main.js
@@ -3,17 +3,38 @@
 
 /**
  * Build very simple blog posting web service server
- *
  * Use RESTful api with HTTP verb
- */
-
-/**
- * Post feature
  *
+ * Post feature:
  * GET /posts
  * GET /posts/:id -> ID specified posts
  * POST /posts
+ *
+ * With a process's memory-based datas
  */
+
+// Datas
+
+/**
+ * @typedef Post
+ * @property {string} id
+ * @property {string} title
+ * @property {string} content
+ */
+
+/** @type {Post[]} */
+const posts = [
+  {
+    id: "id00",
+    title: "First post title",
+    content: "Hello world!",
+  },
+  {
+    id: "id01",
+    title: "나의 두 번째 포스트", // Hangul
+    content: "Foo bar!",
+  },
+];
 
 const http = require("http");
 
@@ -27,24 +48,60 @@ const server = http.createServer((req, res) => {
   const POSTS_ID_REGEXP = /^\/posts\/([a-zA-Z0-9_]+)$/;
 
   // [1] index is captured content
-  const capturedId = req.url && POSTS_ID_REGEXP.exec(req.url)?.[1];
+  const capturedPostsId = req.url && POSTS_ID_REGEXP.exec(req.url)?.[1];
 
   if (req.method === "GET" && req.url === "/posts") {
     // General get post request
 
+    // Return value: All posts
+    const retData = {
+      // Posts key
+      posts: posts.map((post) => ({
+        id: post.id,
+        title: post.title,
+      })),
+      // Object length key
+      totalLength: posts.length,
+    };
+
     // Send response for prevent "response hanging"
     res.statusCode = 200; // [200 ~ 299] HTTP successful response status code
-    res.end("List of posts");
-  } else if (req.method === "GET" && capturedId) {
+
+    res.setHeader("Content-Type", "application/json; charset=utf-8");
+    res.end(JSON.stringify(retData));
+  } else if (req.method === "GET" && capturedPostsId) {
     // ID specifying get post request
 
-    // Print captured id
-    console.log(`Requested ID: ${capturedId}`);
+    // Get id specified post from posts
+    const postById = posts.find((post) => post.id === capturedPostsId);
 
-    res.statusCode = 200;
-    res.end("List of id specified posts");
+    // Check post exists
+    if (postById) {
+      // // Practice code
+      // const idAndContentOnly = postById.map((post) => ({
+      //   id: post.id,
+      //   content: post.content
+      // })}))
+
+      res.statusCode = 200;
+
+      // Key: Value
+      res.setHeader("Content-Type", "application/json; charset=utf-8");
+      res.end(JSON.stringify(postById));
+    } else {
+      // If there is no mathed post
+
+      res.statusCode = 404;
+      res.end("404 Not found");
+    }
   } else if (req.method === "POST" && req.url === "/posts") {
     // General post the post request
+
+    // Listening event
+    req.setEncoding("utf-8"); // By default, returns Buffer object
+    req.on("data", (chunk) => {
+      console.log(chunk);
+    });
 
     res.statusCode = 200;
     res.end("Create the posts");
