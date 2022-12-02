b2ba7e1f34725c32ab618c4e5f72fd4e319ec6f1
Yi Hong-ju <to_hongju@pm.me>
2022-12-02 10:46:29 +0900

    Update module example with absolute path

--- back-end/fastcampus-lecture/ch-06/module-importing/common-js.js
+++ back-end/fastcampus-lecture/ch-06/module-importing/common-js.js
@@ -1,5 +1,5 @@
 // @ts-check
-/* eslint-disable no-console */
+/* eslint-disable no-console, node/no-unpublished-require */
 
 /**
  * require: CommonJS style
@@ -8,12 +8,23 @@
  */
 
 // CommonJS style module import
-console.log(require("../module-example-modules/animals"));
+console.log(require("./node_modules/animals"));
 
-console.log(module.require("../module-example-modules/animals")); // Same works
+console.log(module.require("./node_modules/animals")); // Same works
 
 // Destructuring module object
 console.log("\nTeardown module object");
 
 const { path, paths, filename } = module;
 console.log({ path, paths, filename });
+
+/* Node.js standard lib: Use absolute path */
+const http = require("http");
+
+// Print http module info
+console.log("\nTeardown http module", http);
+
+/* Can use custom absolute path if modules in node_modules */
+const birds = require("birds");
+
+console.log("\nBirds from absolute path:", birds);
