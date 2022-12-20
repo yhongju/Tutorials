a06d0e168d8aad0e670a32fefaa9b4e00a248415
Yi Hong-ju <to_hongju@pm.me>
2022-12-20 18:06:58 +0900

    Update component.tsx and dummy icon file

--- Preact-and-Twindcss/routes/components.tsx
+++ Preact-and-Twindcss/routes/components.tsx
@@ -1,3 +1,67 @@
-const MyButton = (props) => {
-  return <button class="my-button">{props.text}</button>;
+import { createElement, render } from "preact";
+
+// interface {
+
+// }
+
+const MyButton = (props) => <button class="my-button">{props.text}</button>;
+
+const vdom = <MyButton text="Text from `vdom!`" />;
+// It compiled like this
+const vdom_comp = createElement(MyButton, { text: "Text from `vdom`!" });
+
+/**
+ * Nesting components
+ */
+const MediaPlayer = () => {
+  return (
+    <div>
+      <MyButton text="Play" />
+      <MyButton text="Stop" />
+    </div>
+  );
 };
+
+const MediaPlayerCond = (props) => {
+  return (
+    <div>
+      {props.playing ? <MyButton text="Stop" /> : <MyButton text="Play" />}
+    </div>
+  );
+};
+
+// render(vdom, document.body);
+// export default MediaPlayer; // To actually run with fresh
+
+// render(<MediaPlayerCond playing={true} />, document.body);
+// Renders <button>Stop<button>
+
+/**
+ * Component children
+ *
+ * <Foo>
+ *  <a />
+ *  <b />
+ * </Foo>
+ *
+ * const Foo = (props) => {
+ *  return props.child
+ * }
+ * // [<a />, <b />]
+ */
+
+const MyButtonChild = (props) => {
+  // console.log(props.children);
+  return <button class="my-button">{props.children}</button>; // children: Nested component
+};
+const App = () => {
+  return (
+    <MyButtonChild>
+      <img src="icon.png" />
+      Click me!
+    </MyButtonChild>
+  );
+};
+
+// render(<App />, document.body);
+export default App;
